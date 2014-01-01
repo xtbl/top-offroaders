731028de3eea2f42237256439d7e8721a01e4107
xtbl <correodecristobal@gmail.com>
2014-01-01 15:01:41 -0600

    find all cars added

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/cars', cars.findAll);
-app.get('/cars/:id', cars.findById);
+//app.get('/cars/:id', cars.findById);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
