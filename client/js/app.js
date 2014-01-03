13025f4ad69414b3135b99cb42953d3954365191
cavila <cavila@IT-DSK-597.gap.com>
2014-01-03 11:48:46 -0600

    added car detail

--- client/js/app.js
+++ client/js/app.js
@@ -3,5 +3,6 @@
 angular.module('myApp', ['ngRoute','myApp.controllers', 'myApp.services'])
 .config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.when('/cars', {templateUrl: 'partials/car-list.html', controller: 'CarListCtrl'});
+	$routeProvider.when('/cars/:carId', {templateUrl: 'partials/car-detail.html', controller: 'CarDetailCtrl'});
 	$routeProvider.otherwise({redirectTo: '/cars'});
 }]);
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,9 +1,14 @@
 'use strict';
 
 angular.module('myApp.controllers', [])
-	.controller('MainCtrl', ['$scope','Car', function ($scope, Car) {
-		$scope.test = "test";
+	.controller('MainCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
+		$rootScope.go = function (path) {
+			$location.path(path);
+		};
 	}])
 	.controller('CarListCtrl', ['$scope','Car', function ($scope, Car) {
 		$scope.cars = Car.query();
+	}])
+	.controller('CarDetailCtrl', ['$scope', '$routeParams', 'Car', function ($scope, $routeParams, Car) {
+		$scope.car = Car.get({carId: $routeParams.carId});
 	}]);
