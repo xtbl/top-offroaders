59079c41294e3b09df27a91c3a17516460c5a950
cavila <cavila@IT-DSK-597.gap.com>
2014-01-03 15:55:35 -0600

    main page gallery style added, added string filter

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp', ['ngRoute','myApp.controllers', 'myApp.services'])
+angular.module('myApp', ['ngRoute','myApp.controllers', 'myApp.services', 'myApp.filters'])
 .config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.when('/cars', {templateUrl: 'partials/car-list.html', controller: 'CarListCtrl'});
 	$routeProvider.when('/cars/:carId', {templateUrl: 'partials/car-detail.html', controller: 'CarDetailCtrl'});
